3be1a827eb0b22fd3afb7de0146dec37e743344c
Vikash Tiwari <101000395+VikashTiwari22@users.noreply.github.com>
2022-05-05 23:25:16 +0530

    Add files via upload

---
+++ addition of two no.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std ;
+int main ()
+{
+    int a=4 , b=5 ,sum=a+b;
+    cout<<"enter the value of a and b";
+    // cin>>a>>b;
+    sum =a+b;
+    cout<<endl<<sum;
+}
