bd88e98262d60d5be202c68f0f9df692b59072fe
Vikash Tiwari <101000395+VikashTiwari22@users.noreply.github.com>
2022-05-05 23:52:16 +0530

    Add files via upload

---
+++ swapping of two no.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+    int a=2,b=3,temp;
+    temp=a;
+    a=b;
+    b=temp;
+    cout<<"value of a is "<<a<<endl;
+    cout<<"value of b is "<<b;
+}
