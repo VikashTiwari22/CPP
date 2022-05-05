85a29f6838bbe5e4ff73ea71c597623e194f64dc
Vikash Tiwari <101000395+VikashTiwari22@users.noreply.github.com>
2022-05-05 23:36:23 +0530

    Add files via upload

---
+++ sum of two integer.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+    int x,y,sum;
+    cout<<"type a no"<<endl;
+    cin>>x;
+    cout<<"type another no"<<endl;
+    cin>>y;
+    sum=x+y;
+    cout<<"sum is"<<sum;
+}
