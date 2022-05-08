45985eb0fdd03a2309d2cd52494fa895662d9449
Vikash Tiwari <101000395+VikashTiwari22@users.noreply.github.com>
2022-05-08 20:35:53 +0530

    Add files via upload

---
+++ math function.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main ()
+{
+    int a=5,b=7;
+    cout<<max(a,b)<<endl;
+    cout<<sqrt(b)<<endl;
+    cout<<round(5.8)<<endl;
+    cout<<log(a)<<endl;
+    cout<<sin(a);
+    return 0;
+}
