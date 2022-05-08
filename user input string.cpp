516133eb2db9498375b41d48afaaa432201845e6
Vikash Tiwari <101000395+VikashTiwari22@users.noreply.github.com>
2022-05-08 17:10:32 +0530

    Add files via upload

---
+++ user input string.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main ()
+{
+    string firstname;
+    cout<<" your first name is ";
+    cin >>firstname;//get user input from keyboard
+    cout<<"your first name is "<<firstname;
+}
