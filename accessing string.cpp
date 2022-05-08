26689aaf274bf58e23de7dae619456a5f91613c6
Vikash Tiwari <101000395+VikashTiwari22@users.noreply.github.com>
2022-05-08 16:47:07 +0530

    Add files via upload

---
+++ accessing string.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+    string a="dvshgsvfjsfjbfjsdjhf";
+    cout<<a[5]<<endl<<a.size();
+    //you can also use .length() for find the length or size of a string
+    return 0;
+}
