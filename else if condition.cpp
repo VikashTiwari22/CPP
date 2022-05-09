4484c8568877162d426ef4ec75ba47bb0f141299
Vikash Tiwari <101000395+VikashTiwari22@users.noreply.github.com>
2022-05-09 19:39:35 +0530

    Add files via upload

---
+++ else if condition.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+    int a=5;
+    if(a>5)
+    {
+        cout<<"value is greater than 5 "<< a;
+    }
+    else if (a<5)
+    {
+        cout<<"value is less than 5 and value is "<<a;
+    }
+    else
+    {
+        cout<<a;
+    }
+}
