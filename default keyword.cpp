e37cfcc8bb6b83a90176f221c8f0b19093616fcc
Vikash Tiwari <101000395+VikashTiwari22@users.noreply.github.com>
2022-05-09 21:51:37 +0530

    Add files via upload

---
+++ default keyword.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+    int day = 4;
+    switch (day)
+    {
+        case 5:
+        cout<<"friday";
+        break;
+        case 6:
+        cout<<"saturday";
+        break;
+        case 7:
+        cout<<"sunday";
+        break;
+        default:
+        cout<<"looking forward to the weekend";
+    
+    }
+}    
+    
+ 
