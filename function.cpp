2169c3d8228c452e031002754cdef8f96b71c971
Vikash Tiwari <101000395+VikashTiwari22@users.noreply.github.com>
2022-11-08 16:54:02 +0530

    Add files via upload

---
+++ function.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+void mainFunction()  //create a function
+{
+    cout<<"hello";
+}
+int main()
+{
+    mainFunction();  // call the function
+}
+// a function is a block of code it is only run when it is called.
+// void means that the function does not have a return value.
