02cacd9aceab35f8a8b8cbd2cdca79a6320368a7
Vikash Tiwari <101000395+VikashTiwari22@users.noreply.github.com>
2023-02-09 19:43:11 +0530

    Add files via upload

---
+++ square of stars.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int size;
+    cin>>size;
+    for(int i=1;i<=size;i++)
+    {
+        for(int j=1;j<=size;j++)
+        {
+            cout<<"*"<<" ";
+        }
+        cout<<"\n";
+    }
+}
