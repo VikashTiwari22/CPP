b17eb6180093c3c05a2581eab676e5f49db5c89c
Vikash Tiwari <101000395+VikashTiwari22@users.noreply.github.com>
2023-02-18 12:05:28 +0530

    Add files via upload

--- switch case.cpp
+++ switch case.cpp
@@ -1,33 +1,19 @@
 #include<iostream>
 using namespace std;
-int main ()
+int main()
 {
-    int day = 5;
-    switch (day)
+    int x=2;
+    switch(x)
     {
         case 1:
-        cout<<"monday";
-        break; 
+        cout<<"choice is one ";
+        break;
         case 2:
-        cout<<"tuesday";
+        cout<<"choice is 2";
         break;
         case 3:
-        cout<<"wednesday";
+        cout<<"choice is 3";
         break;
-        case 4:
-        cout<<"thursaday";
-        break ;
-        case 5:
-        cout<<"friday";
-        break;
-        case 6:
-        cout<<"saturday";
-        break;
-        case 7:
-        cout<<"sunday";
-        break;
-    
     }
-}    
-    
- 
+    return 0;
+}
